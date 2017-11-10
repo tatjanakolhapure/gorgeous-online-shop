b51cd8b69beacccf031b65c58ce3a898e12ae520
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-11-10 20:54:45 +0000

    add parallax block

--- static/js/custom.js
+++ static/js/custom.js
@@ -24,6 +24,7 @@
 				    fn.homeBanner();
                     // change html for home banner arrows, remove "previous" and "next"
 				    jQuery(".slick-arrow").text("");
+				    fn.parallaxImg();
                 });
 
 				jQuery(window).on("resize", function() {
@@ -34,6 +35,7 @@
             scroll: function() {
 			    jQuery(window).scroll(function() {
 				    fn.changeLogoSize();
+				    fn.parallaxImg();
                 });
             },
 
@@ -170,7 +172,34 @@
                       }
                     }]
                 });
-            }
+            },
+            // code by Renan Breno https://codepen.io/RenanB/pen/GZeBNg
+            parallaxImg: function() {
+			    var img = $('.parallax-img');
+			    var imgParent = img.parent();
+			    var speed = img.data('speed');
+			    var imgY = imgParent.offset().top;
+			    var winY = jQuery(window).scrollTop();
+			    var winH = jQuery(window).height();
+			    var parentH = imgParent.innerHeight();
+			    // The next pixel to show on screen
+                var winBottom = winY + winH;
+                //If block is shown on screen
+                if (winBottom > imgY && winY < imgY + parentH) {
+                    console.log("yes");
+                    // Number of pixels shown after block appear
+                    var imgBottom = ((winBottom - imgY) * speed);
+                    // Max number of pixels until block disappear
+                    var imgTop = winH + parentH;
+                    // Porcentage between start showing until disappearing
+                    var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+                }
+                img.css({
+                    top: imgPercent + '%',
+                    transform: 'translate(-50%, -' + imgPercent + '%)'
+                });
+			}
+			// End of code by Renan Breno
 		};
 
 		fn.init();
