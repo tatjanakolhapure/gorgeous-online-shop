e1fd7177fda04c85a82d69c8c3ad74e6f0d16182
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-11-18 19:55:30 +0000

    add address form on registration page, send responses to ajax when submitting login and registration forms,  add args to account view, create edit address and edit details views

--- accounts/views.py
+++ accounts/views.py
@@ -1,57 +1,111 @@
-from django.contrib import messages, auth
+import json
+
+from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template.context_processors import csrf
-from accounts.forms import UserRegistrationForm, UserLoginForm
+from django.http import JsonResponse, HttpResponse
+from django.db import IntegrityError
+
+from django.conf import settings
+from accounts.models import Address, User
+from accounts.forms import UserRegistrationForm, UserLoginForm, AddressFrom, UserDetailsFrom
 
 def login(request):
     if request.method == 'POST' and 'register' in request.POST:
         form_register = UserRegistrationForm(request.POST)
+        form_address = AddressFrom(request.POST)
 
-        if form_register.is_valid():
-            form_register.save()
+        try:
+            if all([form_register.is_valid(), form_address.is_valid()]):
+                user = form_register.save()
+                address = form_address.save(commit=False)
+                address.user = user
+                address.save()
 
-            user = auth.authenticate(email=request.POST.get('email'),
-                                     password=request.POST.get('password1'))
+                user = auth.authenticate(email=request.POST.get('email'),
+                                         password=request.POST.get('password1'))
+                if user:
+                    auth.login(request, user)
+                    return JsonResponse({"status":"200"})
+                else:
+                    return JsonResponse({"message": "Your email or password was not recognised"}, status=404)
 
-            if user:
-                messages.success(request, "You have successfully registered")
-                return redirect(reverse('account'))
-
-            else:
-                messages.error(request, "unable to log you in at this time!")
+        except IntegrityError:
+            return JsonResponse({"message": "User with this email already exists"}, status=404)
+        else:
+            return JsonResponse({"message": "Passwords do not match"}, status=404)
 
     else:
         form_register = UserRegistrationForm()
+        form_address = AddressFrom()
 
     if request.method == 'POST' and 'login' in request.POST:
+
         form_login = UserLoginForm(request.POST)
         if form_login.is_valid():
             user = auth.authenticate(email=request.POST.get('email'),
                                      password=request.POST.get('password'))
-
             if user is not None:
                 auth.login(request, user)
-                messages.error(request, "You have successfully logged in")
-                return redirect(reverse('account'))
+                return JsonResponse({"status": "200"})
             else:
-                form_login.add_error(None, "Your email or password was not recognised")
+                return JsonResponse({"message": "Your email or password was not recognised"}, status=404)
 
     else:
         form_login = UserLoginForm()
 
-    args = {'form_register': form_register, 'form_login': form_login}
+    args = {'form_register': form_register, 'form_address': form_address, 'form_login': form_login}
     args.update(csrf(request))
     return render(request, 'login.html', args)
 
-
 @login_required(login_url='/login/')
 def account(request):
-    return render(request, 'account.html')
+    user = request.user
+    try:
+        address = get_object_or_404(Address, user=user)
+    except Exception as e:
+        address = None
+    return render(request, 'account.html', {'address': address})
 
 
 def logout(request):
     auth.logout(request)
-    messages.success(request, 'You have successfully logged out')
     return redirect(reverse('home'))
+
+
+@login_required
+def edit_address(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    address = get_object_or_404(Address, user=user)
+
+    if request.method == "POST":
+        form = AddressFrom(request.POST, instance=address)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('account'))
+    else:
+        form = AddressFrom(instance=address)
+
+    args = {'form': form}
+    args.update(csrf(request))
+
+    return render(request, 'account_address_form.html', args)
+
+@login_required
+def edit_details(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+
+    if request.method == "POST":
+        form = UserDetailsFrom(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('account'))
+    else:
+        form = UserDetailsFrom(instance=user)
+
+    args = {'form': form}
+    args.update(csrf(request))
+
+    return render(request, 'account_details_form.html', args)
