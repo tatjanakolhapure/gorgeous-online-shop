8bf9a5d2eee1ee5d49e8ad5487199f93bfafc5d1
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-11-18 21:42:14 +0000

    add created field for product and change field type for amount field, run migration

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
@@ -18,14 +19,14 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     size = models.ManyToManyField('Size')
     color = models.ManyToManyField('Color')
+    created = models.DateTimeField(default=timezone.now)
 
 class Image(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images")
 
 class Stock(models.Model):
-    amount = models.IntegerField()
+    amount = models.PositiveIntegerField()
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     size = models.ForeignKey('Size', on_delete=models.CASCADE)
     color = models.ForeignKey('Color', on_delete=models.CASCADE)
-
