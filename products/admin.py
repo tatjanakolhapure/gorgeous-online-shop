c51912ecc65eb1312677ff057dfd7a9604752626
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-11-18 23:00:49 +0000

    add list display for each model, add filters and list editable for product

--- products/admin.py
+++ products/admin.py
@@ -3,10 +3,30 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Category, Product, Size, Color, Stock
+from .models import Category, Product, Size, Color, Image, Stock
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Size)
-admin.site.register(Color)
-admin.site.register(Stock)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category']
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'created']
+    list_filter = ['category__category', 'size__size', 'color__color', 'created']
+    list_editable = ['price']
+admin.site.register(Product, ProductAdmin)
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['size']
+admin.site.register(Size, SizeAdmin)
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['color']
+admin.site.register(Color, ColorAdmin)
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'image', 'product']
+admin.site.register(Image, ImageAdmin)
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'product', 'size', 'color']
+admin.site.register(Stock, StockAdmin)
