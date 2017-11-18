0ff1c83547e7b4cf6d60910fa588b52ad69d975a
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-11-18 21:22:51 +0000

    add models to admin site

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, Size, Color, Stock
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Size)
+admin.site.register(Color)
+admin.site.register(Stock)
