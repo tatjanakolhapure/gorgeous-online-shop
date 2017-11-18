41719922ac908858ffdbb6cf06d376d3f563d279
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-11-18 23:02:31 +0000

    update field names, add unique together for stock, add ordering and __unicode__ functions

--- products/models.py
+++ products/models.py
@@ -4,13 +4,31 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
-    name = models.CharField(max_length=40)
+    category = models.CharField(max_length=40)
+
+    class Meta:
+        ordering = ['category']
+
+    def __unicode__(self):
+        return self.category
 
 class Size(models.Model):
-    name = models.CharField(max_length=20)
+    size = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ['size']
+
+    def __unicode__(self):
+        return self.size
 
 class Color(models.Model):
-    name = models.CharField(max_length=20)
+    color = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ['color']
+
+    def __unicode__(self):
+        return self.color
 
 class Product(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
@@ -21,12 +39,27 @@ class Product(models.Model):
     color = models.ManyToManyField('Color')
     created = models.DateTimeField(default=timezone.now)
 
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
 class Image(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, null=True)
     image = models.ImageField(upload_to="images")
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
 
 class Stock(models.Model):
     amount = models.PositiveIntegerField()
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     size = models.ForeignKey('Size', on_delete=models.CASCADE)
     color = models.ForeignKey('Color', on_delete=models.CASCADE)
+
+    unique_together = ('product', 'size', 'color')
