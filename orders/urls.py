1f13872d8b8df93925ace8fc47c9f795e3222b91
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-12-04 17:47:59 +0000

    add url to orders list

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^checkout/$', views.checkout, name='checkout'),
-    # url(r'^$', views.orders_list, name='orders_list'),
+    url(r'^$', views.orders_list, name='orders_list'),
     url(r'^(?P<order_id>\d+)/$', views.order, name='order'),
 ]
 
