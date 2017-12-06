4ce6af6128fee16866795c304de5911741cee443
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-12-06 21:47:06 +0000

    update database info

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,11 @@
+import dj_database_url
 from base import *
 
 DEBUG = False
 
+# Load the ClearDB connection details from the environment variable
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 # Stripe environment variables
