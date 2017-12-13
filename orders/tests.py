7fc781fe9826e5b30838193bf64e8f6e4319f60b
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-12-13 11:52:08 +0000

    test payment form and stripe charge

--- orders/tests.py
+++ orders/tests.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import datetime
 
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.shortcuts import render_to_response
+from django.conf import settings
 
 from accounts.models import User
 from .views import *
+from .forms import PaymentForm
 
 class OrderListPageTest(TestCase):
 
@@ -92,3 +95,101 @@ class CheckoutPageTest(TestCase):
 
     def test_checkout_page_uses_correct_template(self):
         self.assertTemplateUsed(self.checkout_page, "orders/checkout.html")
+
+class PaymentFormTest(TestCase):
+
+    def setUp(self):
+        self.expiry_year = str(datetime.datetime.today().year+1)[-2:]
+
+    def test_payment_form_month_choices(self):
+        choices = [
+            ('', 'MM'),
+            ('1', '01'), ('2', '02'), ('3', '03'),
+            ('4', '04'), ('5', '05'), ('6', '06'),
+            ('7', '07'), ('8', '08'), ('9', '09'),
+            ('10', '10'), ('11', '11'), ('12', '12'),
+        ]
+        form = PaymentForm()
+        self.assertEqual(choices, form.fields['expiry_month'].choices)
+
+    def test_payment_form_year_choices(self):
+        choices = [
+            ('', 'YY'),
+            (17, 17), (18, 18), (19, 19),
+            (20, 20), (21, 21), (22, 22),
+            (23, 23), (24, 24), (25, 25),
+        ]
+        form = PaymentForm()
+        self.assertEqual(choices, form.fields['expiry_year'].choices)
+
+    def test_payment_form(self):
+        form = PaymentForm({
+            'address_line1': '10 House Name',
+            'address_line2': '21 Street Name',
+            'address_city': 'Test City',
+            'address_zip': 'SSS',
+            'name': 'Jason Smith',
+            'card_number': '4242424242424242',
+            'expiry_month': '2',
+            'expiry_year': self.expiry_year,
+            'cvv': '333',
+            'stripe_id': 'teststripe478',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_payment_form_fails_without_expiry_month(self):
+        form = PaymentForm({
+            'address_line1': '10 House Name',
+            'address_line2': '21 Street Name',
+            'address_city': 'Test City',
+            'address_zip': 'SSS',
+            'name': 'Jason Smith',
+            'card_number': '4242424242424242',
+            'expiry_year': self.expiry_year,
+            'cvv': '333',
+            'stripe_id': 'teststripe478',
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_payment_form_without_stripe_id(self):
+        form = PaymentForm({
+            'address_line1': '10 House Name',
+            'address_line2': '21 Street Name',
+            'address_city': 'Test City',
+            'address_zip': 'SSS',
+            'name': 'Jason Smith',
+            'card_number': '4242424242424242',
+            'expiry_month': '2',
+            'expiry_year': self.expiry_year,
+            'cvv': '333',
+        })
+        self.assertTrue(form.is_valid())
+
+class StripeChargeTest(TestCase):
+
+    stripe.api_key = settings.STRIPE_SECRET
+
+    def setUp(self):
+        expiry_year = str(datetime.datetime.today().year + 1)[-2:]
+        # get token for creating a charge
+        self.token = stripe.Token.create(
+            card={
+                'number': '4242424242424242',
+                'exp_month': '6',
+                'exp_year': expiry_year,
+                'cvc': '123',
+            })
+
+    def test_stripe_create_and_update_charge(self):
+        charge = stripe.Charge.create(
+            amount=7500,
+            currency="GBP",
+            card=self.token
+        )
+        # check if charge has been paid
+        self.assertEqual(charge.paid, True)
+        description = 'Order 1 for Jason Smith, jason.smith@example.com'
+        charge.description = description
+        charge.save()
+        #check if description has been updated
+        self.assertEqual(charge.description, description)
